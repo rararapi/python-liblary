7ac117fe12d52c5834853223e96b0238d93b06ce
rapirapi <bot0obok9hvu@gmail.com>
2023-05-04 17:13:15 +0900

    二項係数mod pを追加

--- liblary.py
+++ liblary.py
@@ -60,6 +60,24 @@ import math
 def combr(n, r):
     return math.comb(n + r - 1, r)
 
+# 二項係数をmod pで
+def cmb(n, r, mod):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n-r] % mod
+mod = 10 ** 9 + 7
+N = 10 ** 6  # N は必要分だけ用意する
+fact = [1, 1]  # fact[n] = (n! mod p)
+factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod p)
+inv = [0, 1]  # factinv 計算用
+ 
+for i in range(2, N + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    factinv.append((factinv[-1] * inv[-1]) % mod)
+
+
 # フェニック木
 class Fenwick_Tree:
     def __init__(self, n):
