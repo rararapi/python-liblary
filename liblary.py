c6e86e1dce49399e37aec306c9da13027473435d
rapirapi <bot0obok9hvu@gmail.com>
2023-07-02 01:03:17 +0900

    pow追加

--- liblary.py
+++ liblary.py
@@ -1193,3 +1193,17 @@ def query(u, v):
         fu, fv = fv, fu
     return S[min(_query(fu, fv+1))[1]]
 
+# 繰り返し二乗法を用いたpow
+def pow(A, B, p):
+    res = 1  # Initialize result
+    A = A % p  # Update A if it is more , than or equal to p
+    if (A == 0):
+        return 0
+    while (B > 0):
+        if ((B & 1) == 1):  # If B is odd, multiply, A with result
+            res = (res * A) % p
+ 
+        B = B >> 1  # B = B/2
+        A = (A * A) % p
+    return res
+
