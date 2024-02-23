991d15964ac9bdaa249a633062e8e1bb8f8ba6d3
rapirapi <bot0obok9hvu@gmail.com>
2024-02-23 23:41:34 +0900

    区間加算・区間和取得するBIP追加

--- liblary.py
+++ liblary.py
@@ -107,6 +107,43 @@ class Fenwick_Tree:
             r -= r & -r
         return s
 
+# 区間加算・区間和取得するBIP
+class RangeBIT:    
+    def __init__(self, n):
+        class BIT:
+            def __init__(self, n):
+                self.size = n
+                self.arr = [0] * (n + 1)
+
+            def __getitem__(self, i):
+                return self.sum(i + 1) - self.sum(i)
+
+            def sum(self, i):
+                s = 0
+                tmp = i
+                while tmp:
+                    s += self.arr[tmp]
+                    tmp -= tmp & -tmp
+                return s
+
+            def add(self, i, x):
+                tmp = i + 1
+                while tmp <= self.size:
+                    self.arr[tmp] += x
+                    tmp += tmp & -tmp
+        self.bit0 = BIT(n)
+        self.bit1 = BIT(n)
+
+    def add(self, i, j, x):
+        self.bit0.add(i, -x * i)
+        self.bit0.add(j, x * j)
+        self.bit1.add(i, x)
+        self.bit1.add(j, -x)
+
+    def sum(self, i, j):
+        si = self.bit0.sum(i) + self.bit1.sum(i) * i
+        sj = self.bit0.sum(j) + self.bit1.sum(j) * j
+        return sj - si
 
 # メモ化
 from functools import lru_cache
