ae97e3462dd6b9bceed52e8e52cbd943f27ad715
rapirapi <bot0obok9hvu@gmail.com>
2024-07-20 19:19:24 +0900

    Z algorithm追加

--- liblary.py
+++ liblary.py
@@ -956,3 +956,29 @@ def extended_gcd(a, b):
         x = y1 - (b // a) * x1
         y = x1
         return gcd, x, y
+
+# Z algorithm
+def z_algorithm(s):
+    '''
+    入力された文字列またはリストのZ配列を返す
+    '''
+
+    if isinstance(s, str):
+        s = [ord(c) for c in s]
+
+    n = len(s)
+    if n == 0:
+        return []
+
+    z = [0] * n
+    j = 0
+    for i in range(1, n):
+        z[i] = 0 if j + z[j] <= i else min(j + z[j] - i, z[i - j])
+        while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+            z[i] += 1
+        if j + z[j] < i + z[i]:
+            j = i
+    z[0] = n
+
+    return z
+
